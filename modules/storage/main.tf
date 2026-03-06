6f5e1061f1fda8e12098d7f8b82461f5b6dc08a1
balasubramaniyand2203 <balasubramaniyand2203@gmail.com>
2026-03-06 14:32:32 +0530

    Create main.tf

---
+++ modules/storage/main.tf
@@ -0,0 +1,18 @@
+resource "azurerm_storage_account" "tfstate" {
+
+  name                     = "tfstateprod123"
+  resource_group_name      = var.rg_name
+  location                 = var.location
+
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+}
+
+resource "azurerm_storage_container" "tfstate" {
+
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.tfstate.name
+  container_access_type = "private"
+
+}
